7265ed7f71b48fa145e8fbb31e19acbef839cb9a
sogutemir <108363656+sogutemir@users.noreply.github.com>
2023-12-24 23:33:01 +0300

    Update İndex

---
+++ footer.php
@@ -0,0 +1,61 @@
+<footer class="site-footer">
+    <div class="container">
+        <div class="row">
+            <div class="footer-about col-md-4">
+                <h3>Hakkımızda</h3>
+                <p>Biz, müşterilerimize en iyi hizmeti sunmayı hedefleyen bir ilan platformuyuz.</p>
+            </div>
+            <div class="footer-nav col-md-4">
+                <h3>Navigasyon</h3>
+                <ul>
+                    <li><a href="index.php">Ana Sayfa</a></li>
+                    <li><a href="adverts.php">İlanlar</a></li>
+                    <li><a href="contact.php">İletişim</a></li>
+                    <!-- Diğer navigasyon linkleri -->
+                </ul>
+            </div>
+            <div class="footer-contact col-md-4">
+                <h3>İletişim</h3>
+                <ul>
+                <li><a href="mailto:info@example.com">muhammedsogut@stu.aydin.edu.tr</a></li>
+                    <li><a href="mailto:info@example.com">kadir.ugurlu@stu.aydin.edu.tr</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 text-center">
+                <p>&copy; <?= date("Y"); ?> IAU Computer Engineering Tüm hakları saklıdır.</p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- footer stil tanımlamaları -->
+<style>
+    .site-footer {
+        background-color: #f8f9fa;
+        color: #333;
+        padding: 40px 0;
+        border-top: 1px solid #e7e7e7;
+    }
+    .site-footer ul {
+        list-style: none;
+        padding: 0;
+    }
+    .site-footer ul li a {
+        color: #333;
+        text-decoration: none;
+    }
+    .site-footer ul li a:hover {
+        text-decoration: underline;
+    }
+    .footer-about, .footer-nav, .footer-contact {
+        margin-bottom: 20px;
+    }
+    .footer-about h3, .footer-nav h3, .footer-contact h3 {
+        margin-bottom: 10px;
+    }
+    .text-center {
+        text-align: center;
+    }
+</style>
--- index.php
+++ index.php
@@ -6,22 +6,120 @@
     <title>Ana Sayfa</title>
     <?php include 'head.php'; ?>
 </head>
+<style>
+.carousel-item img {
+  max-height: 500px; /* Maksimum yükseklik değerini sayfanın yapısına göre ayarlayın */
+  width: auto; /* Genişliği otomatik ayarla, aspect ratio korunur */
+  margin: auto; /* Yatayda merkezleme yapar */
+}
+
+#featuredCarousel {
+  max-height: 500px; 
+  overflow: hidden; 
+}
+        </style>
+
 <body>
     <?php include_once("navbar.php"); ?>
     
     <?php
-
-    // Giriş durumunu kontrol et
-    if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
-        echo '<p>Hoş geldiniz, ' . htmlspecialchars($_SESSION['username']) . '!</p>';
-    } else {
-        // Kullanıcı giriş yapmamışsa, giriş sayfasına yönlendir
-        // header('Location: login.php');
-        // exit();
-        echo "<p>Giriş yapmadınız.</p>";
+    if (!isset($_SESSION)) {
+        session_start();
     }
-    ?>
+    // Giriş durumunu kontrol et
+    if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true): ?>
+        <header class="main-header">
+            <div class="container">
+                <h1>Hoş Geldiniz!</h1>
+                <p>Bizimle birlikte keşfedin, ilan verin ve bulun.</p>
+                <a href="register.php" class="btn btn-primary">Kayıt Ol</a>
+                <a href="login.php" class="btn btn-secondary">Giriş Yap</a>
+            </div>
+        </header>
+    <?php endif; ?>
 
-    <p>Hello, World!</p>
+<?php
+// Veritabanı bağlantı detayları
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "advertphp";
+
+// Bağlantıyı oluştur
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Bağlantıyı kontrol et
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Öne çıkan ilanlar için SQL sorgusu
+$sql = "SELECT advert.id, advert.title, advert.description, advert.price, 
+               (SELECT photo FROM advert_photo WHERE advert_photo.advert_id = advert.id LIMIT 1) AS photo
+        FROM advert
+        ORDER BY advert.id DESC
+        LIMIT 10"; // Örnek olarak son 10 ilanı alabiliriz.
+
+$result = $conn->query($sql);
+
+// SQL sorgusu sonuçlarını bir diziye aktaralım
+$featuredAdverts = [];
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $featuredAdverts[] = $row;
+    }
+}
+
+
+?>
+
+    <section class="featured-adverts">
+        <div class="container">
+            <h2>Öne Çıkan İlanlar</h2>
+            <div id="featuredCarousel" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+        <?php foreach ($featuredAdverts as $key => $advert): ?>
+            <div class="carousel-item <?php echo ($key == 0) ? 'active' : ''; ?>">
+                <a href="/AdvertSitePhp/advert-detail.php?id=<?php echo $advert['id']; ?>&title=<?php echo urlencode($advert['title']); ?>">
+                    <img src="data:image/jpeg;base64,<?php echo base64_encode($advert['photo']); ?>" class="d-block w-100" alt="<?php echo htmlspecialchars($advert['title']); ?>">
+                </a>
+                <div class="carousel-caption d-none d-md-block">
+                    <h5><?php echo htmlspecialchars($advert['title']); ?></h5>
+                </div>
+            </div>
+        <?php endforeach; ?>
+
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#featuredCarousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#featuredCarousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+        </div>
+    </section>
+
+    <section class="how-it-works">
+        <div class="container">
+            <h2>Nasıl Çalışır?</h2>
+            <p>Sitemizde ilan vermek veya ilanlara göz atmak oldukça kolay. İşte adımlar:</p>
+            <!-- Adımları açıklayan içerik -->
+        </div>
+    </section>
+
+    <footer>
+        <div class="container">
+            <?php include 'footer.php'; ?>
+        </div>
+    </footer>
+
+    <!-- Opsiyonel JavaScript -->
+    <!-- jQuery, Popper.js, Bootstrap JS gibi -->
+    <script src="path_to_jquery"></script>
+    <script src="path_to_popper"></script>
+    <script src="path_to_bootstrap_js"></script>
 </body>
 </html>
